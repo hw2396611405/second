50343473dc123ccacc57c60038375fbbd7b5fd8a
hw <2396611405@qq.com>
2016-01-27 14:29:35 +0800

    初建

---
+++ today/today/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  today
+//
+//  Created by wanghui on 16/1/27.
+//  Copyright © 2016年 王辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ today/today/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  today
+//
+//  Created by wanghui on 16/1/27.
+//  Copyright © 2016年 王辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
