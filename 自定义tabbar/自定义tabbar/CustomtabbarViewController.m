7c72f76c3e6cc3efc85f99831c0c4cb5fb6e7f25
hw <2396611405@qq.com>
2016-01-29 09:41:56 +0800

    分修改

--- 自定义tabbar/自定义tabbar/CustomtabbarViewController.m
+++ 自定义tabbar/自定义tabbar/CustomtabbarViewController.m
@@ -8,9 +8,15 @@
 
 #import "CustomtabbarViewController.h"
 
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width
+#define kHeight 60  // 下方item视图的高度
+#define kHeightOfBottomView kHeight - 12  // 显示的下方视图的高度
+
 @interface CustomtabbarViewController ()
 @property (nonatomic,strong)NSArray *viewControllers;//s数组,用来存放存进来的VC
-
+@property (nonatomic,strong)NSMutableArray *btnArr;//button存放的数组
+@property (nonatomic,strong)NSMutableArray *labelArr;//label的数组
 @end
 
 @implementation CustomtabbarViewController
@@ -19,6 +25,8 @@
     self = [super init];
     if (self) {
         self.viewControllers = [NSArray arrayWithArray:controllers];
+        self.btnArr = [NSMutableArray arrayWithCapacity:1];
+        self.labelArr = [[NSMutableArray alloc]init];
         
         //布局下方子视图
         [self setSubView];
@@ -30,7 +38,39 @@
 
 //布局下方子视图
 - (void)setSubView {
-
+    //创建底层透明的View
+    UIView *view = [[UIView alloc]initWithFrame:CGRectMake(0, kScreenHeight - kHeight, kScreenWidth, kHeight)];
+    view.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:view];
+    //创建底层不透明的小一点的View
+    UIView *bottomView = [[UIView alloc]initWithFrame:CGRectMake(0, kScreenHeight - kHeightOfBottomView, kScreenWidth, kHeightOfBottomView)];
+    
+    bottomView.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:bottomView];
+    
+    float width = kScreenWidth /self.viewControllers.count;
+    
+    //创建每个item的button和label
+    for (int i = 0; i < self.viewControllers.count; i++) {
+        //创建button
+        UIButton *btn =[UIButton buttonWithType:UIButtonTypeCustom];
+        btn.frame = CGRectMake((width - 25) / 2 + width * i, 17, 25, 25);
+        btn.tintColor = [UIColor blackColor];
+        [view addSubview:btn];
+        [self.btnArr addObject:btn];
+        // btn的点击事件
+        [btn addTarget:self action:@selector(handleAction:) forControlEvents:UIControlEventTouchUpInside];
+        
+        // 创建label
+        UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(5 + width * i, 42, width - 10, kHeightOfBottomView - 30)];
+        label.text = @"test";
+        label.textAlignment = NSTextAlignmentCenter;
+        [view addSubview:label];
+        [self.labelArr addObject:label];
+    }
+    
+    
+    
 
 }
 
