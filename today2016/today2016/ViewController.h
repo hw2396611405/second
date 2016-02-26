167ecbf37735b557e583a42d9ef878012a05d3f5
hw <2396611405@qq.com>
2016-02-26 14:30:33 +0800

    初建

---
+++ today2016/today2016/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  today2016
+//
+//  Created by wanghui on 16/2/26.
+//  Copyright © 2016年 王辉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ today2016/today2016/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  today2016
+//
+//  Created by wanghui on 16/2/26.
+//  Copyright © 2016年 王辉. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
